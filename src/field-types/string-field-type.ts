6b7112e28d51b1b7ca9602face8677c7fbbd7790
Francis Enyi <39003759+geekhybrid@users.noreply.github.com>
2022-03-12 22:05:41 +0000

    Create string-field type validator

---
+++ src/field-types/string-field-type.ts
@@ -0,0 +1,64 @@
+import { Extremes, Field, FieldValidator, ModelValidationCollection } from "../types";
+import { useIsEmail } from ".././validation-actions/is-email";
+import { useIsRequired } from "../validation-actions/is-required";
+import { useIsEquals } from "../validation-actions/is-equals";
+import { useHasLength } from "../validation-actions/has-length";
+import { useLimitedLength } from "../validation-actions/has-min-max-length";
+
+
+export type StringValidator<TModel> = Field<TModel> & {
+    isRequired: () => StringValidator<TModel>;
+    isEquals: (comparer: string) => StringValidator<TModel>;
+    isEmail: () => StringValidator<TModel>;
+    hasLength: (fixedLength: number) => StringValidator<TModel>;
+    hasMinLength: (minLength: number) => StringValidator<TModel>;
+    hasMaxLength: (maxLength: number) => StringValidator<TModel>;
+}
+
+export const createStringValidator = <TModel extends Record<string, any>>(
+    model: TModel,
+    property: string,
+    validations: ModelValidationCollection): FieldValidator<string, TModel> => {
+
+    if (!validations[property]) {
+        validations[property] = [];
+    }
+
+    const _validator = {} as StringValidator<TModel>;
+    _validator.isRequired = () => {
+        validations[property].push(useIsRequired)
+        return _validator;
+    }
+    
+    _validator.isEquals = (comparer: string) => {
+        validations[property].push(() => useIsEquals(model, property, comparer))
+        return _validator;
+    }
+
+    _validator.isEmail = () => {
+        validations[property].push(useIsEmail)
+        return _validator;
+    }
+
+    _validator.hasLength = (fixedLen: number) => {
+        validations[property].push(() => useHasLength(model, property, fixedLen))
+        return _validator;
+    }
+
+    _validator.hasMinLength = (minLength: number) => 
+    {
+        validations[property].push(
+            () => useLimitedLength(model, property, { limit: minLength, type: Extremes.minimum })
+        );
+        return _validator;
+    }
+
+    _validator.hasMaxLength = (maxLength: number) => 
+    {
+        validations[property].push(
+            () => useLimitedLength(model, property, { limit: maxLength, type: Extremes.maximum })
+        );return _validator;
+    }
+
+    return _validator as FieldValidator<string, TModel>;
+}
