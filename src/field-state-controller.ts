93b8868dd5bac8294c0750e82944a599f1becbf2
Francis Enyi <39003759+geekhybrid@users.noreply.github.com>
2022-03-19 23:30:10 +0000

    Added controller for checking field validity

---
+++ src/field-state-controller.ts
@@ -0,0 +1,45 @@
+import { ref } from 'vue';
+import { FieldStateController, FieldStates, FieldValidity } from './types';
+
+const setAllFieldsToUntouchedState = <TModel>(instance: TModel, fieldStates: FieldStates<TModel>) => {
+    Object.keys(instance).forEach((field) => {
+        fieldStates[field as unknown as keyof TModel] = {
+            isUntouched: true,
+            isValid: false,
+            notValid: false,
+        };
+    });
+};
+
+const areAllFieldsValid = <TModel>(fieldStates: FieldStates<TModel>) => {
+    return Object.values<FieldValidity>(fieldStates).every((field: FieldValidity) => field.isValid === true);
+};
+
+const hasUntouchedFields = <TModel>(fieldStates: FieldStates<TModel>) => {
+    return Object.values<FieldValidity>(fieldStates).some((field: FieldValidity) => field.isUntouched === true);
+};
+
+const set = <TModel>(field: keyof TModel, validity: FieldValidity, fieldStates: FieldStates<TModel>) => {
+    fieldStates[field] = validity;
+};
+
+export const useFieldStateController = <TModel>(
+    instance: TModel,
+): {
+    fieldStateController: FieldStateController;
+    fieldStates: FieldStates<TModel>;
+} => {
+    const fieldStates = {} as FieldStates<TModel>;
+    setAllFieldsToUntouchedState(instance, fieldStates);
+
+    const controller = {} as FieldStateController;
+    controller.areAllFieldsValid = () => areAllFieldsValid(fieldStates);
+    controller.hasUntouchedFields = () => hasUntouchedFields(fieldStates);
+    controller.set = (field: string, validity: FieldValidity) =>
+        set(field as unknown as keyof TModel, validity, fieldStates);
+
+    return {
+        fieldStateController: ref<FieldStateController>(controller).value,
+        fieldStates: fieldStates,
+    };
+};
---
+++ test/field-state-controller.ts
@@ -0,0 +1,57 @@
+import { useFieldStateController } from '../src/field-state-controller';
+import { useTestModel } from './model';
+import assert from 'assert';
+import { FieldValidity } from '../src/types';
+
+describe('field-state-controller', () => {
+    const testModel = useTestModel();
+
+    it('initialises fields to untouched state', () => {
+        const { fieldStateController, fieldStates } = useFieldStateController(testModel);
+
+        assert.equal(fieldStateController.hasUntouchedFields(), true);
+        assert.equal(
+            Object.values<FieldValidity>(fieldStates).every((field) => field.isUntouched),
+            true,
+        );
+    });
+
+    it('areAllFieldsValid should return true if all fields are valid', () => {
+        const { fieldStateController } = useFieldStateController(testModel);
+        Object.keys(testModel).forEach((field) =>
+            fieldStateController.set(field, {
+                isValid: true,
+                isUntouched: false,
+                notValid: false,
+            }),
+        );
+
+        assert.equal(fieldStateController.areAllFieldsValid(), true);
+    });
+
+    it('hasUntouchedFields should return true if any fields is in untouched state', () => {
+        const { fieldStateController } = useFieldStateController(testModel);
+        Object.keys(testModel).forEach((field) =>
+            fieldStateController.set(field, {
+                isValid: false,
+                isUntouched: true,
+                notValid: false,
+            }),
+        );
+
+        assert.equal(fieldStateController.hasUntouchedFields(), true);
+    });
+
+    it('set should set field validity', () => {
+        const expectedValidity = {
+            isValid: false,
+            isUntouched: true,
+            notValid: false,
+        };
+
+        const { fieldStateController, fieldStates } = useFieldStateController(testModel);
+        fieldStateController.set('age', expectedValidity);
+
+        assert.deepEqual(fieldStates.age, expectedValidity);
+    });
+});
