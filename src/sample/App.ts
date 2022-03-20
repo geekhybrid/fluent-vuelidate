dc17ce4da2c919094759263f2d7d4e2570914214
Francis Enyi <39003759+geekhybrid@users.noreply.github.com>
2022-03-20 10:27:02 +0000

    Updated sample application

--- src/sample/App.ts
+++ src/sample/App.ts
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref, watch } from 'vue';
 import { useValidator } from '..';
 type LoginCredentials = {
     userName: string;
@@ -6,21 +6,30 @@ type LoginCredentials = {
 };
 export default defineComponent({
     setup() {
-        const credentials = {
+        const credentials = ref<LoginCredentials>({
             userName: '',
             password: '',
-        };
-        const validator = useValidator<LoginCredentials>(credentials)
-            .for('password').isRequired().next
-            .for('userName').isEmail();
+        });
+
+        // const validator = useValidator<LoginCredentials>(credentials.value)
+        //     .for('password').isRequired('Password is required').next
+        //     .for('userName').isEmail('Please enter a valid email.');
+
+        //Multi-rules
+        const validator = useValidator<LoginCredentials>(credentials.value)
+            .for('password')
+                .isRequired('Password is required')
+                .hasMinLength(5, 'Password must have length of 5 characters.').next
+            .for('userName').isEmail('Please enter a valid email.');
 
         const submit = () => {
-            if (validator.isValid.value) {
+            if (validator.isValid.value === true) {
                 console.log('Is Valid');
             } else {
                 console.log('is invalid');
             }
         };
+
         return {
             header: 'Fluent-Vuelidate Sample',
             form: validator.fields,
--- src/sample/App.vue
+++ src/sample/App.vue
@@ -1,8 +1,26 @@
 <template>
+    <div>
+        Completely unappealing login form to demonstrate validations.
+    </div>
     <h1>{{ header }}</h1>
+    <label>Username</label>
     <input v-model="credentials.userName" style="width: 500px" />
-    <small v-if="form.userName.notValid">Invalid user name</small>
+    <div v-if="form.userName.notValid">
+        <p class="error" v-for="error in form.userName.errors" :key="error">{{  error }}</p>
+    </div>
+    <br>
+
+    <label>Password</label>
+    <input v-model="credentials.password" style="width: 500px" />
+    <div v-if="form.password.notValid">
+        <p class="error" v-for="error in form.password.errors" :key="error">{{  error }}</p>
+    </div>
     <button @click="submit">Submit</button>
 </template>
 
 <script lang="ts" src="./App.ts" />
+<style scoped>
+    .error {
+        color: red;
+    }
+</style>
